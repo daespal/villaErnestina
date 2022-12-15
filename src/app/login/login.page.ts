881fbb9baa2fe6bed29a0a1a6e7d4e04774baee3
Andrés Zurita <ananzuritabi@ittepic.edu.mx>
2022-12-14 22:38:32 -0700

    Login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,10 @@
+import { Reservacion } from './../models/reservacion';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { ReservacionService } from '../services/reservacion.service';
+import { NavController } from '@ionic/angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,95 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  
+  public admi:Administrador[];
+  public usuario:Reservacion[];
+  public huesped:Reservacion[];
+  public myForm: FormGroup;
+  public codigo:string="";
 
-  constructor() { }
+
+  constructor(private router: Router,private alertController: AlertController, 
+    private service:ReservacionService, private fb: FormBuilder, private navCtrl: NavController) {
+      this.service.getReservacion().subscribe(res => {
+        this.huesped = res;
+        console.log(this.huesped)
+      });
+  }
 
   ngOnInit() {
+  }
+  async loginPrueba (){
+    this.service.huespedes = this.huesped;
+    this.huesped.forEach(item=>{
+      this.service.codigoAdm.push(item.codigo)
+    });
+    this.service.validarToken(this.myForm.get('codigo').value);
+    this.service.getHuespedByCodigo(this.myForm.get('codigo').value)
+  }
+  public buscarUsuario(){
+    this.codigo = this.myForm.controls.codigo.value;
+  
+    console.log(this.codigo);
+    
+    this.service.getAdmin().subscribe(res => {
+      this.admi = res;
+      console.log(this.admi)
+      this.admi.forEach(i=>{
+
+        if (i.codigo.localeCompare(this.codigo)==0 ){
+          console.log("si existe")
+          this.ingresarAdmi();
+          //your awesome code here
+        } else {
+          this.buscarHost()
+          console.log("no existe");
+                    
+          //your awesome code here
+        }  
+    });
+
+    });
+
+   
+  }
+
+
+  public buscarHost(){
+    this.service.getReservacion().subscribe(res => {
+      this.usuario = res;
+      console.log(this.usuario)
+      this.usuario.forEach(i=>{
+
+        if (i.codigo.localeCompare(this.codigo)==0 ){
+          console.log("si existe")
+          this.ingresarHost();
+          this.getUsuarioByCodigo(this.codigo);
+          //your awesome code here
+        } else {
+          console.log("no existe");
+          //your awesome code here
+        }  
+    });
+  });
+  }
+
+  public ingresarAdmi(){
+    this.router.navigate(['list-host'])
+  }
+
+  public ingresarHost(){
+    this.router.navigate(['view-huesped'])
+  }
+
+ 
+
+  public getUsuarioByCodigo(codigo:string){
+    this.router.navigate(['tap-huesped'],{
+      queryParams: {codigo:codigo}
+     
+    });
+    console.log(this.codigo);
   }
 
 }
--- src/app/services/reservacion.service.ts
+++ src/app/services/reservacion.service.ts
@@ -1,9 +1,70 @@
+
 import { Injectable } from '@angular/core';
+import { Administrador } from './../models/administrador';
+import { Reservacion } from '../models/reservacion';
+import { NavigationExtras, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservacionService {
+  public huespedes: Reservacion[];
 
-  constructor() { }
+  public codigoAdm = ['admin'];
+  public codigo = '';
+  constructor(private firestore: AngularFirestore, private authFirebase: AngularFireAuth, private router:Router) { }
+
+  validarToken(token: string){
+    if (token === this.codigoAdm[0]){
+      this.router.navigate(['/list-host']);
+    }else if(this.codigoAdm.includes(token)){
+      let navigation: NavigationExtras = {
+        state:{
+          codigoAdm: token
+        }
+      }
+      this.router.navigate(['/tap-huesped/view-huesp'], navigation)
+    }
+
+  }
+
+  public getHuespedByCodigo(token: string){
+    this.huespedes.forEach(item =>{
+      if(item.codigo === token){
+        this.huesped = item;
+      }
+    })
+  }
+  public getReservacion(): Observable<Reservacion[]> {
+    return this.firestore.collection('Reservacion').snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          console.log(a);
+          const data = a.payload.doc.data() as Reservacion;
+          console.log(data);
+          const id = a.payload.doc.id;
+          return { id, ...data }
+        });
+      })
+    );
+  }
+  public getAdmin(): Observable<Administrador[]> {
+    return this.firestore.collection('Administrador').snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          console.log(a);
+          const data = a.payload.doc.data() as Administrador;
+          console.log(data);
+          const id = a.payload.doc.id;
+          return { id, ...data }
+        });
+      })
+    );
+  }
 }
